6292bd427976e97ed4830fb44f8427ba60985b62
Itss_SMAC <196476326+Its-SMAC@users.noreply.github.com>
2026-06-12 14:10:58 +0100

    Serve frontend templates and static assets

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -8,8 +9,19 @@ import (
 
 func main() {
 	r := gin.Default()
+	r.LoadHTMLGlob("web/templates/*")
+	r.Static("/static", "./web/static")
 
 	r.GET("/health", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"status": "Api is fine"})
 	})
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.tmpl", gin.H{"Titulo": "Ip Checker"})
+	})
+
+	fmt.Println("Server has been start.")
+	fmt.Println("Server run on 127.0.0.1:8080")
+
+	r.Run(":8080")
 }
